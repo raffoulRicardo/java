1e81571a4856a7b8d07835cc516e959f8fc1a80f
raffoulRicardo <ricardo_raffoul@hotmail.com>
2019-03-28 20:37:52 -0300

    while and exercices

---
+++ src/course/Question16.java
@@ -0,0 +1,20 @@
+package course;
+
+import java.util.Scanner;
+
+public class Question16 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int password = sc.nextInt();
+
+        while (password != 2002) {
+            System.out.println("Senha Invalida");
+            password = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+
+        sc.close();
+    }
+}
