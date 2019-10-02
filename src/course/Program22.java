c041431b866a8d5e521f6e411f7a3404cdc06830
raffoulRicardo <ricardo_raffoul@hotmail.com>
2019-10-01 22:30:01 -0300

    static method

---
+++ src/course/Program22.java
@@ -0,0 +1,10 @@
+package course;
+
+public class Program22 {
+    public static void main(String[] args) {
+
+        for (double i = 0; i < 500; i++) {
+            System.out.println(0.01 + 0.02 == 0.03);
+        }
+    }
+}
